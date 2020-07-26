61418e182f16679d94fe990109660e2d7a774e61
AB1974 <bekemaylin@gmail.com>
2020-07-26 10:31:30 -0400

    screenshotadded

--- src/test/java/com/bitrix/runners/CukesRunner.java
+++ src/test/java/com/bitrix/runners/CukesRunner.java
@@ -14,7 +14,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-        plugin = "html:target/cucumber-report.html",//creates report
+        plugin = {"html:target/cucumber-report.html",
+        "json:target/cucumber.json",
+        "rerun:target/rerun.txt" },//creates report
         features = "src/test/resources/features",//features content root
         glue = "com/bitrix/step_definitions", //glue source root
         dryRun = false,
--- src/test/java/com/bitrix/step_definitions/Hook.java
+++ src/test/java/com/bitrix/step_definitions/Hook.java
@@ -1,9 +1,11 @@
 package com.bitrix.step_definitions;
 
+import com.bitrix.utilities.BrowserUtils;
 import com.bitrix.utilities.Configuration_Reader;
 import com.bitrix.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 
 public class Hook {
 
@@ -14,11 +16,13 @@ public class Hook {
 
 
     }
-
-    @After
-    public void tearDown() {
-
-        Driver.closeDriver();
+@After
+    public void tearDown(Scenario scenario){
+        //if test failed, take a screenshot
+        if( scenario.isFailed()){
+            BrowserUtils.takeScreenshot();
+        }
+       // Driver.closeDriver();
     }
 
 

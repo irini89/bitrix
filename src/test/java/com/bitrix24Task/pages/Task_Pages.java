2e5ef8fa344844a4c2dc1838da9d7665f19b2ecc
Sulaimon20 <sulaimon_s@yahoo.com>
2020-08-03 09:24:51 -0400

    added some correction

--- src/test/java/com/bitrix24Task/pages/Task_Pages.java
+++ src/test/java/com/bitrix24Task/pages/Task_Pages.java
@@ -1,6 +1,6 @@
 package com.bitrix24Task.pages;
 
-import com.task_module.utilities.Driver;
+import com.bitrix24Task.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
